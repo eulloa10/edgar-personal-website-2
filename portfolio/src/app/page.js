7c655e98a237d75965b3a5d0d7026f413bb72c4c
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2023-11-28 14:53:48 -0800

    updated skills section

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -1,11 +1,13 @@
 import Nav from "./components/nav";
 import About from "./components/about";
+import Skills from "./components/skills";
 
 export default function Main() {
   return (
     <>
       <Nav />
       <About />
+      <Skills />
     </>
   )
 }
