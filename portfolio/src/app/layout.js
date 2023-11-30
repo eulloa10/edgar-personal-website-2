005002a7d0f921ecbd95a6176e189e2bb78e8750
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2023-11-30 01:52:45 -0800

    added Google analytics

--- portfolio/src/app/layout.js
+++ portfolio/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} strategy="lazyOnload"></Script>
+    <Script strategy="lazyOnload" id="gtag">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+      `}
+    </Script>
       <body className={inter.className}>{children}</body>
     </html>
   )
