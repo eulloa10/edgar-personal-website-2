d3542f5e9fa03a0a40257c931d8bda91c2b16b2b
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2023-11-30 00:40:41 -0800

    added favicon

--- portfolio/src/app/components/about.js
+++ portfolio/src/app/components/about.js
@@ -13,7 +13,7 @@ export default function About() {
       </div>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Software engineer.</h2>
+          <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Software engineer.</h1>
           <p className="mt-6 text-lg leading-8 text-white">
             I enjoy the journey from idea to code, and from code to a practical, efficient product.
           </p>
