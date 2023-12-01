e544ec4e64e14152795404f4fc3e9c9973f09c8d
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2023-11-30 19:16:00 -0800

    update google tags

--- portfolio/src/app/layout.js
+++ portfolio/src/app/layout.js
@@ -13,14 +13,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
+        <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
         <Script id="google-analytics">
-          {`
+            {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
+
             gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-          `}
+            `}
         </Script>
         {children}
       </body>
