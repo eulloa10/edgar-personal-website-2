af9fdfc990f6b964ebbab776d23ad599d935dd5a
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2024-01-28 00:37:59 -0800

    updated html

--- portfolio/src/app/components/about.js
+++ portfolio/src/app/components/about.js
@@ -35,12 +35,6 @@ export default function About() {
             className="inline-flex items-center rounded-md bg-white px-10 py-2 text-sm font-medium text-black ring-1 ring-inset ring-gray-500/10 mt-10">
               View Resume
           </a>
-          <a
-            href="https://drive.google.com/file/d/10u4LZqLVkNwgmT8EGJku3tGmt3tXAijq/view?usp=sharing" target="_blank"
-            rel="noopener noreferrer"
-            className="inline-flex items-center rounded-md bg-white px-10 py-2 text-sm font-medium text-black ring-1 ring-inset ring-gray-500/10 mt-10">
-              View Resume
-          </a>
         </div>
       </div>
     </div>
