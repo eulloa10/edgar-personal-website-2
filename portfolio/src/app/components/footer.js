308b98c788c9244f00849d14d6ecd9d89ceb29c1
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2024-03-26 19:49:48 -0700

    updated footer

--- portfolio/src/app/components/footer.js
+++ portfolio/src/app/components/footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-white text-black p-4 border-t border-black">
       <div className="container mx-auto flex items-center justify-center">
-        <p className="mr-2">&copy; 2023 Edgar Ulloa</p>
+        <p className="mr-2">&copy; 2024 Edgar Ulloa</p>
         <a href="https://github.com/eulloa10" target="_blank" rel="noopener noreferrer" className="mx-2">
           <Image
             src="/github-mark.png"
